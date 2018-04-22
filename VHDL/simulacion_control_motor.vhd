fd337d3477c7574e0494df46eb888db751326bb9
Carlos Sanchez <30807442+sanchezco@users.noreply.github.com>
2018-04-23 01:45:15 +0200

    Update simulacion_control_motor.vhd

--- VHDL/simulacion_control_motor.vhd
+++ VHDL/simulacion_control_motor.vhd
@@ -1,43 +1,68 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 22.04.2018 15:01:06
--- Design Name: 
--- Module Name: simulacion_control_motor - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+entity simulacion_motor is
+end simulacion_motor;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+architecture Behavioral of simulacion_motor is
 
-entity simulacion_control_motor is
---  Port ( );
-end simulacion_control_motor;
-
-architecture Behavioral of simulacion_control_motor is
-
+    Component control_motor  --Defino entradas y salidas
+    Port(
+     direccion_sw: in STD_LOGIC;
+      enable_sw:   in STD_LOGIC;
+      reset:    in STD_LOGIC;
+      clk:      in STD_LOGIC;
+      
+      DIR:        out STD_LOGIC;
+      STEP:       out STD_LOGIC;
+      ENABLE: out STD_LOGIC
+      ); 
+     end component;
+     
+  --Señales intermedias
+     signal direccion_s:  std_logic;
+     signal enable_sw_s :    std_logic;
+     signal reset_s :     std_logic;
+     signal clk_s :       std_logic;
+     
+     signal DIR_s:        std_logic;
+     signal STEP_s:       std_logic;
+     signal ENABLE_s:     std_logic;
+     
 begin
 
+  UnidadEnPruebas: control_motor
+  Port Map (
+  -- puertos componente => senales del banco de pruebas
+      direccion_sw    => direccion_s,
+      enable_sw       => enable_sw_s,
+      reset        => reset_s,
+      clk          => clk_s,
+      DIR          => DIR_s,
+      STEP         => step_s,
+      ENABLE       => enable_s
+  );
 
+  Reloj: Process  -- la frecuencia de la FPGA es de 100MHz, periodo de clk 10ns (5ns high y 5ns low)
+  begin  
+      clk_s <= '1';
+      wait for 5ns;
+      clk_s <= '0';
+      wait for 5ns;
+      
+    end process;
+     
+    
+ Estimulos: Process
+     begin 
+     reset_s <= '1';
+     wait for 50ns;
+     reset_s <= '0';
+     direccion_s <= '0';
+     enable_sw_s <= '1';
+     wait;
+     end process;
+    
+    
 end Behavioral;
