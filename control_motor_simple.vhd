196500672bb190add4955a630e063e3ac14619e3
Carlos Sanchez <30807442+sanchezco@users.noreply.github.com>
2018-04-23 01:37:12 +0200

    Update control_motor_simple.vhd

--- control_motor_simple.vhd
+++ control_motor_simple.vhd
@@ -3,32 +3,28 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;-- para poder utilizar signed y unsigned;
 
-
---Las seÒales de los interruptores se activan a nivel alto, inicialmente estan a 0 y al cambiar el interruptor se activan.
-
+--Las se√±ales de los interruptores se activan a nivel alto
 entity control_motor is
   Port (
   --entradas:
-  direccion_sw : in STD_LOGIC; --SW_1:  cambia la direccion                    
+  direccion_sw : in STD_LOGIC; --SW_1:  cambia la direcci√≥n                    
      enable_sw : in std_LOGIC; --SW_0:  activa el motor con el interruptor 
          reset : in STD_LOGIC; --SW_15: activa el reset con el interruptor      
            clk : in STD_LOGIC; -- 100MHz por lo tanto el reloj es de 10ns
---salidas:                     -SeÒal acitva a nivel bajo, a cambiar el interruptor 
+--salidas:                    
             DIR : out STD_LOGIC;  
            STEP : out STD_LOGIC;  
          ENABLE : out STD_LOGIC  
   );
 end control_motor;
 
-
 architecture Behavioral of control_motor is
-        signal ta: unsigned(8 downto 0); --puedo contar hasta 2048 ( para hacer la cuenta contando hasta 2000ns : 2microseg, siendo el minimo 1 microseg)
+        signal ta: unsigned(8 downto 0); --puedo contar hasta 512 (siendo el pulso m√≠nimo de 1 microseg, 500*10ns=5microseg)
         signal cuenta_step: unsigned(8 downto 0); 
-        signal step_in: std_logic; -- variable intermedia que da el valor a la salida STEP
+        signal step_in: std_logic;   -- variable intermedia que da el valor a la salida STEP
         signal enable_in: std_logic; -- variable intermedia que da el valor a la salida ENABLE
 
 begin
-
 --///////////////////////////// PROCESO PARA OBTENER ENABLE ////////////////////////////////////
 proceso_enable: process (clk, reset)
 begin
@@ -43,8 +39,6 @@ begin
     end if;
 end process;
        ENABLE <= enable_in;
-----/////////////////////////////////////////////////////////////////////////////////////////////
- 
 --///////////////////////////// PROCESO PARA OBTENER DIR ////////////////////////////////////////
 proceso_DIR: process (clk, reset)
 begin
@@ -58,10 +52,8 @@ begin
        end if;
     end if;
 end process;
-
 --///////////////////////////// TIEMPO ENTRE PASOS //////////////////////////////////////////////
     ta <= to_unsigned(500,9); 
-
 --///////////////////////////////////// PROCESO PARA EL STEP ////////////////////////////////////
  Proceso_paso : process (clk,reset)
   begin
@@ -71,7 +63,7 @@ end process;
     
     elsif clk' event and clk = '1' then 
      if enable_in = '1' then
-                    if cuenta_step = ta-1 then  -- hay una seÒal step cada ta
+                    if cuenta_step = ta-1 then  -- hay una se√±al step cada ta
                         cuenta_step <= (others =>'0');
                         step_in <= not step_in;
                      else
@@ -81,7 +73,5 @@ end process;
        end if;
   end process;
    STEP <= step_in;
-   
-
+ 
     end Behavioral;
-
